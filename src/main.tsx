f21f07f9b12d5765f6c6f80893685fecc2768332
Nakib Hasan Jhalak <stdjhalak@gmail.com>
2025-09-19 09:55:02 +0600

    done

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,10 @@ import { store } from './redux/store.ts'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <CustomCursor />
+      
+      <div className="hidden md:block">
+        <CustomCursor />
+      </div>
       <RouterProvider router={router}></RouterProvider>
     </Provider>
   </StrictMode>,
